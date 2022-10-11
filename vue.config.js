c962f0e31111a5450b71e94f4bbc19019909e399
khanhbuivcc <khanhbuiduy@vccorp.vn>
2022-10-12 00:29:12 +0700

    update

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/' + process.env.CI_PROJECT_NAME + '/'
+    ? '/demo-pwa/'
     : '/',
   transpileDependencies: true,
   pwa: {
