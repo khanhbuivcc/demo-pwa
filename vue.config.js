70da099671d7b50eb180abcba4078347de975def
khanhbuivcc <khanhbuiduy@vccorp.vn>
2022-10-12 22:44:03 +0700

    update

--- vue.config.js
+++ vue.config.js
@@ -4,24 +4,30 @@ module.exports = defineConfig({
   publicPath: '/demo-pwa/',
   transpileDependencies: true,
   pwa: {
-    name: 'Demo pwa app',
+    name: 'app cua khanh',
     themeColor: '#749F82',
     msTileColor: '#425F57',
     manifestOptions: {
       icons: [
-        { "src": "./img/logo.png", "sizes": "192x192", "type": "image/png" },
-        { "src": "./img/logo.png", "sizes": "512x512", "type": "image/png" },
         {
-          "src": "./img/logo.png",
-          "sizes": "192x192",
-          "type": "image/png",
-          "purpose": "maskable"
+            "src": "./img/icon-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
         },
         {
-          "src": "./img/logo.png",
-          "sizes": "512x512",
-          "type": "image/png",
-          "purpose": "maskable"
+            "src": "./img/icon-256x256.png",
+            "sizes": "256x256",
+            "type": "image/png"
+        },
+        {
+            "src": "./img/icon-384x384.png",
+            "sizes": "384x384",
+            "type": "image/png"
+        },
+        {
+            "src": "./img/icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
         }
       ],
     }
