525de99e9b2b3e7a49c3939be7e34d3cb17782fb
khanhbuivcc <khanhbuiduy@vccorp.vn>
2022-10-13 01:06:20 +0700

    update

---
+++ firebase-messeging-sw.js
@@ -0,0 +1,28 @@
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js')
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js')
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB-xqr23XUxi-p9bqYgjtRWBPTsXdV6s-g",
+  authDomain: "pwa-push-message.firebaseapp.com",
+  projectId: "pwa-push-message",
+  storageBucket: "pwa-push-message.appspot.com",
+  messagingSenderId: "409350048234",
+  appId: "1:409350048234:web:ea3c3f3ab11b8915a20a93",
+  measurementId: "G-HJ9KHB28X6"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  console.log('Received background message ', payload);
+
+  const { title, body } = payload.notification;
+
+  self.registration.showNotification(
+    title,
+    { body },
+  );
+});
