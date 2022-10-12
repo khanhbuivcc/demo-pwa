112a8bc08bc18ad39d6824b80d6522e757d61649
khanhbuivcc <khanhbuiduy@vccorp.vn>
2022-10-13 01:15:33 +0700

    update

--- firebase-messeging-sw.js
+++ firebase-messeging-sw.js
@@ -1,5 +1,6 @@
-importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js')
-importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js')
+// Scripts for firebase and firebase messaging
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
 
 const firebaseConfig = {
   apiKey: "AIzaSyB-xqr23XUxi-p9bqYgjtRWBPTsXdV6s-g",
